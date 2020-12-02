1c56d251e73cb73432492082f4787b6987aa7f9b
william-dvlpr <william.dvlpr@gmail.com>
2020-12-02 13:12:09 -0300

    + atrasos

--- exercises/ex039.py
+++ exercises/ex039.py
@@ -5,14 +5,17 @@ print('=' * 5, 'ALISTAMENTO MILITAR', '=' * 5 )
 
 nascimento = int(input('Em que ano você nasceu? '))
 
-if (atual - nascimento) < 18: # '( )' redundante, p/ facilitar leitura
-    print('Você ainda vai se alistar.')
+idade = atual - nascimento
+
+if idade < 18: 
+    print(f'Você ainda vai se alistar, faltam {18 - idade} ano(s).')
     print(f'Ano de alistamento: {nascimento + 18}, com 18 anos.')
 
-elif (atual - nascimento) == 18:
+elif idade == 18:
     print('Você deve se alistar este ano.')
     print(f'Ano de alistamento: {nascimento + 18}, com 18 anos.')
 
 else:
-    print('Você já deveria ter se alistado.')
-    print(f'Ano de alistamento: {nascimento + 18}, com 18 anos')
+    print(f'Você já deveria ter se alistado, está {idade - 18} ano(s) atrasado.')
+    print(f'Ano de alistamento: {nascimento + 18}, com 18 anos.')
+    
